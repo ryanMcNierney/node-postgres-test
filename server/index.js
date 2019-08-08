5fe6a964e9cfb5d20e526c47be7bcf4dd84c0858
ryanMcNierney <ryan.mcnierney@gmail.com>
2019-08-08 11:17:29 -0400

    Add public index.html

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use('/api', require('./api'))
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
 })
 
 app.listen(PORT, () => {
