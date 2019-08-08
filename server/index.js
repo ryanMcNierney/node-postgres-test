32cfe1097479b84d7f19c7dd507e156abae1f69f
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-08-07 22:44:12 -0400

    Prepare PORT for Heroku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const morgan = require('morgan')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 // dotenv
 require('dotenv').config()
